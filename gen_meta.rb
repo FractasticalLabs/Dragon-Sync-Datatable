cc58a93462658f218235a8ed027e4e91953cf433
Joel Dietz <jdietz+github@gmail.com>
2012-01-20 15:47:30 +0100

    i

--- gen_meta.rb
+++ gen_meta.rb
@@ -1,8 +1,14 @@
 
 class Gen_Meta 
 
-            meta_template = File.open("/Users/jdietz/tech/Dragon-Breath/META", "r")
-            meta_template_contents = meta_template.sysread(meta_template.stat.size)
+            class_template = File.open("/Users/jdietz/tech/class_template-B/META/CLASS", "r")
+            class_template_contents = class_template.sysread(class_template.stat.size)
+            resource_template = File.open("/Users/jdietz/tech/Dragon-B/META/RESOURCE", "r")
+            resource_template_contents = resource_template.sysread(resource_template.stat.size)
+            component_template = File.open("/Users/jdietz/tech/Dragon-B/META/COMPONENT", "r")
+            component_template_contents = component_template.sysread(component_template.stat.size)
+            page_template = File.open("/Users/jdietz/tech/Dragon-B/META/PAGE", "r")
+            page_template_contents = page_template.sysread(page_template.stat.size)
 
             puts "Iterating through all "  + Dir["src/classes/*.cls"].size.to_s + " classes"
             counter = 0
@@ -13,9 +19,46 @@ class Gen_Meta
                   counter += 1
                   puts meta + " does not exist, attempting to create"
                   File.open(meta, 'w') do |f|
-                    f.puts(meta_template_contents)
+                    f.puts(class_template_contents)
                   end
             end
+
+            puts "Iterating through all "  + Dir["src/staticresources/*.resource"].size.to_s + " resources"
+
+            Dir["src/staticresources/*.resource"].each do |file|
+              meta = "#{file}-meta.xml"
+              next if file =~ /-meta\.xml$/ || File.exist?(meta)
+                  counter += 1
+                  puts meta + " does not exist, attempting to create"
+                  File.open(meta, 'w') do |f|
+                    f.puts(resource_template_contents)
+                  end
+            end
+
+            puts "Iterating through all "  + Dir["src/components/*.component"].size.to_s + " components"
+
+            Dir["src/components/*.component"].each do |file|
+              meta = "#{file}-meta.xml"
+              next if file =~ /-meta\.xml$/ || File.exist?(meta)
+                  counter += 1
+                  puts meta + " does not exist, attempting to create"
+                  File.open(meta, 'w') do |f|
+                    f.puts(component_template_contents)
+                  end
+            end
+
+            puts "Iterating through all "  + Dir["src/pages/*.page"].size.to_s + " pages"
+
+            Dir["src/pages/*.page"].each do |file|
+              meta = "#{file}-meta.xml"
+              next if file =~ /-meta\.xml$/ || File.exist?(meta)
+                  counter += 1
+                  puts meta + " does not exist, attempting to create"
+                  File.open(meta, 'w') do |f|
+                    f.puts(page_template_contents)
+                  end
+            end
+
             if counter==0 
                puts "No files found without metadata."
             end
--- update.sh
+++ update.sh
@@ -3,7 +3,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Removing previous files"
-rm -r $DIR/src/*
+rm -r $DIR/src
+mkdir src
+mkdir src/classes
+mkdir src/staticresources
+mkdir src/pages
+mkdir src/components
 echo "Updating all submodules"
 # Not sure that this line is necessary
 git submodule update
@@ -16,8 +21,8 @@ do
 	git pull
 
 	cp -R *.cls $DIR/src/classes
-	cp -R *.resource $DIR/src/resources
-	cp -R *.component $DIR/src/component
+	cp -R *.resource $DIR/src/staticresources
+	cp -R *.component $DIR/src/components
 	cp -R *.page $DIR/src/pages
 
 	cd $DIR
